2cee3d7250ef632a237d6484661106374aa20ba7
pedrodiamel <pedrodiamel@gmail.com>
2018-08-18 13:46:45 -0300

    update

--- runs/createdataset.sh
+++ runs/createdataset.sh
@@ -1,6 +1,6 @@
 
 
-#kaggle competitions download -c tgs-salt-identification-challenge
+#kaggle competitions download -c tgs-salt-identification-challenge -p .kaggle/competitions/tgs-salt-identification-challenge/
 PATH=~/.kaggle/competitions/tgs-salt-identification-challenge/
 unzip $PATH/train.zip -d train
 unzip $PATH/test.zip -d test
